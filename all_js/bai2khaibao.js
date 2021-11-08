5c655d183cbf519f31c65784915e343fc529311c
DuongVanTrung31 <trung.duong.dt31@gmail.com>
2021-11-08 17:02:34 +0700

    bài khai báo

---
+++ all_js/bai2khaibao.js
@@ -0,0 +1,7 @@
+
+// bài 2 tính diện tích hình chữ nhật
+let width = 20;
+let height = 10;
+let area = width * height;
+
+document.write('Area = ' + area);
---
+++ all_js/bai3khaibao.js
@@ -0,0 +1,7 @@
+
+
+// bài 3 dùng prompt() để check a % b === 0
+let a = + prompt('Nhập số a');
+let b = + prompt('Nhập số b');
+let result = a % b === 0 ?'a là bội của b':'a ko chia hết cho b';
+alert(result);
