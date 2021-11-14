06c934138530b122172bb82c952e8b7e6907508f
DuongVanTrung31 <trung.duong.dt31@gmail.com>
2021-11-14 17:57:29 +0700

    removed 2 file .js empty

--- all_js/luyentapdk.js
+++
@@ -1,6 +0,0 @@
-function chiahet(a, b) {
-    let a = document.getElementById('soA').value;
-    let b = document.getElementById('soB').value;
-    let result = a % b === 0 ? 'Có chia hết' : 'Ko chia hết';
-}
-document.getElementById('result').innerHTML = result;
